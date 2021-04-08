b646102661552959118bdc5579ec4624d751c1b7
Mewmaster99 <67807691+Mewmaster99@users.noreply.github.com>
2021-04-08 15:48:00 -0500

    1st build

---
+++ index.js
@@ -0,0 +1,22 @@
+const { Plugin } = require('powercord/entities');
+const { get } = require('powercord/http');
+module.exports = class Nhentai extends Plugin {
+  startPlugin () {
+    powercord.api.commands.registerCommand({
+      command: 'nh',
+      description: 'Generates a random nhentai link',
+      usage: '{c} <your search>',
+      executor: (args) => ({
+          send: true,
+          result: this.nh(args)
+      })
+    });
+  }
+  pluginWillUnload () {
+    powercord.api.commands.unregisterCommand('nh');
+  }
+
+  nh(args) {
+    return `https://nhentai.net/g/${Math.floor(Math.random()*400000)}`
+  }
+}
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Powercord Nhentai link generator
+
+This is a powercord plugin to generate random nhentai links if you're horny and have no clue what you want to look at.
+
+Just type .nh and hit enter and your random nhentai link will have generated.
+
+(side note this will ocasionally give broken links as i made the rng go past the latest "sauce" so don't work if the link doesn't embed or work properly)
